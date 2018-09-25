0532ac85805e490f9045a5ab7569645efd919992
UFP\ssmith37 <ssmith37@uccs.edu>
2018-09-25 00:29:20 -0600

    no message

---
+++ ExtraC++/Chapter8x/Project1/Project1/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+//8-6
+void ProblemSix() {
+	ifstream infile ("TestFile.txt");
+	string instr;
+}
+
+//8-7
+
+//8-13
+
+int main() {
+	ProblemSix();
+	char c;
+	cin >> c;
+	return 0;
+}
